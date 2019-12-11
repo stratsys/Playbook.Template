2f6a26d1ecd6c08e7a2eb8d36851f537f645f3ae
Johan Lindvall <johan.lindvall@stratsys.se>
2019-12-11 11:42:42 +0100

    Add GITHUB_PLAYBOOK_BRANCH parameter.

--- build/update_playbook.sh
+++ build/update_playbook.sh
@@ -3,15 +3,15 @@ source build/common.sh
 
 [[ -z "$PLAYBOOK_SERVICE" ]] && exit 0
 
-( [[ -z "$GITHUB_PLAYBOOK_TOKEN" ]] || [[ -z "$GITHUB_PLAYBOOK_URL" ]] || [[ -z "$GITHUB_PLAYBOOK_EMAIL" ]] || [[ -z "$GITHUB_PLAYBOOK_AUTHOR" ]] ) &&
-   echo "GITHUB_PLAYBOOK_TOKEN, GITHUB_PLAYBOOK_URL, GITHUB_PLAYBOOK_EMAIL and GITHUB_PLAYBOOK_AUTHOR must be set." &&
+( [[ -z "$GITHUB_PLAYBOOK_TOKEN" ]] || [[ -z "$GITHUB_PLAYBOOK_URL" ]] || [[ -z "$GITHUB_PLAYBOOK_BRANCH" ]] || [[ -z "$GITHUB_PLAYBOOK_EMAIL" ]] || [[ -z "$GITHUB_PLAYBOOK_AUTHOR" ]] ) &&
+   echo "GITHUB_PLAYBOOK_TOKEN, GITHUB_PLAYBOOK_URL, GITHUB_PLAYBOOK_BRANCH, GITHUB_PLAYBOOK_EMAIL and GITHUB_PLAYBOOK_AUTHOR must be set." &&
    exit 1
 
 docker run --rm \
 -v "$(pwd)/playbook:/playbook" \
 -w /playbook \
 "$CONTAINER_REGISTRY/stratsys-envhandler:latest" \
---destination dev \
+--destination "$GITHUB_PLAYBOOK_BRANCH" \
 --localSource /playbook \
 --imageTag "$IMAGE_TAG" \
 --token "$GITHUB_PLAYBOOK_TOKEN" \
