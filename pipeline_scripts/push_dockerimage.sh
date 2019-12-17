2679c3d02f12a430bf97c59b49743ea8025a6191
Johan Lindvall <johan.lindvall@stratsys.se>
2019-12-17 15:25:13 +0100

    Fix push of latest.

--- pipeline_scripts/push_dockerimage.sh
+++ pipeline_scripts/push_dockerimage.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source pipeline_scripts/common.sh
 set -x
-docker push "$IMAGE_TAG" && ([[  -z "$IMAGE_TAG_LATEST" ]] || docker push "$IMAGE_TAG_LATEST")
+docker push "$IMAGE_TAG" 
+if [[ ! -z "$IMAGE_TAG_LATEST" ]]; then
+  docker push "$IMAGE_TAG_LATEST"
+fi
