c1942e4769bb770926d7cdfce164d4d0001147bb
Johan Lindvall <johan.lindvall@stratsys.se>
2019-12-17 15:08:51 +0100

    Fix build script.

--- pipeline_scripts/build_docker.sh
+++ pipeline_scripts/build_docker.sh
@@ -3,6 +3,8 @@ source pipeline_scripts/common.sh
 LATEST_TAG_ARG=""
 [[ ! -z "$IMAGE_TAG_LATEST" ]] && LATEST_TAG_ARG="-t "$IMAGE_TAG_LATEST""
 set -x
-[[ ! -z "$DOCKER_BUILD_ARGS" ]] && docker build --rm -t "$IMAGE_TAG" $LATEST_TAG_ARG "${DOCKER_BUILD_ARGS[@]}" .
-[[ -z "$DOCKER_BUILD_ARGS" ]] && docker build --rm -t "$IMAGE_TAG" $LATEST_TAG_ARG .
-
+if [[ -z "$DOCKER_BUILD_ARGS" ]] ; then
+  docker build --rm -t "$IMAGE_TAG" $LATEST_TAG_ARG .
+else
+  docker build --rm -t "$IMAGE_TAG" $LATEST_TAG_ARG "${DOCKER_BUILD_ARGS[@]}" .
+fi
