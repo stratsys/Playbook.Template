108587dd8b697dc65e896973cd1f6172642b4f92
Johan Lindvall <48712038+Johan-Lindvall-Stratsys@users.noreply.github.com>
2020-10-22 12:47:45 +0200

    Update build_config.sh

--- build_config.sh
+++ build_config.sh
@@ -3,4 +3,4 @@ CONTAINER_REGISTRY=stratsys.azurecr.io
 CONTAINER_REGISTRY_REPOSITORY=my-service
 PLAYBOOK_SERVICE=my-stack:my-service
 PUSH_LATEST=0
-#declare -a DOCKER_BUILD_ARGS=("--build-arg" "source_feeds=$FEEDS")
+#DOCKER_BUILD_ARGS="--build-arg source_feeds=$FEEDS"
